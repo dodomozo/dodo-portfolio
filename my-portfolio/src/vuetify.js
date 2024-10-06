b699d06852a14832ff5b9000bcdc967265d4e94b
Dodo <johnreymozo17@gmail.com>
2024-10-06 08:16:06 -0700

    :+1: setup base environment

---
+++ my-portfolio/src/vuetify.js
@@ -0,0 +1,10 @@
+// src/vuetify.js
+import { createVuetify } from 'vuetify';
+import 'vuetify/styles'; // Import Vuetify styles
+import '@mdi/font/css/materialdesignicons.css'; // Import Material Design Icons
+
+const vuetify = createVuetify({
+  // Vuetify options
+});
+
+export default vuetify;
